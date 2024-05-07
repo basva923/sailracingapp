e3e1e2dc2a51a6f821f8276bb5ac95ea0a6efba0
Bas Van Assche <basvanassche@gmail.com>
2024-05-07 22:35:38 +0200

    Showing device orientation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,26 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'sailracingapp';
+
+  protected absolute = false;
+  protected alpha: number | null = 0;
+  protected beta: number | null = 0;
+  protected gamma: number | null = 0;
+
+  constructor() {
+    window.addEventListener('deviceorientation', this.handleOrientation, true);
+  }
+
+  handleOrientation(event: DeviceOrientationEvent) {
+    this.absolute = event.absolute;
+    this.alpha = event.alpha;
+    this.beta = event.beta;
+    this.gamma = event.gamma;
+
+    // Do stuff with the new orientation data
+  }
 }
