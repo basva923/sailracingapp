808303cb8975bded89a53aa25abea140ccf703b1
Bas Van Assche <basvanassche@gmail.com>
2024-05-08 23:02:04 +0200

    Fixed heading function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,12 +23,42 @@ export class HomeComponent {
 
   handleOrientationChange(event: DeviceOrientationEvent) {
     this.absolute = event.absolute;
-    if (event.alpha != null) this.alpha = Math.round(event.alpha);
-    if (event.beta != null) this.beta = Math.round(event.beta);
-    if (event.gamma != null) this.gamma = Math.round(event.gamma);
+    if (event.alpha != null) this.alpha = event.alpha;
+    if (event.beta != null) this.beta = event.beta;
+    if (event.gamma != null) this.gamma = event.gamma;
   }
 
   get heading() {
-    return (360 - this.alpha).toFixed(0);
+    // Convert degrees to radians
+    var alphaRad = this.alpha * (Math.PI / 180);
+    var betaRad = this.beta * (Math.PI / 180);
+    var gammaRad = this.gamma * (Math.PI / 180);
+
+    // Calculate equation components
+    var cA = Math.cos(alphaRad);
+    var sA = Math.sin(alphaRad);
+    var cB = Math.cos(betaRad);
+    var sB = Math.sin(betaRad);
+    var cG = Math.cos(gammaRad);
+    var sG = Math.sin(gammaRad);
+
+    // Calculate A, B, C rotation components
+    var rA = -cA * sG - sA * sB * cG;
+    var rB = -sA * sG + cA * sB * cG;
+    var rC = -cB * cG;
+
+    // Calculate compass heading
+    var compassHeading = Math.atan(rA / rB);
+
+    // Convert from half unit circle to whole unit circle
+    if (rB < 0) {
+      compassHeading += Math.PI;
+    } else if (rA < 0) {
+      compassHeading += 2 * Math.PI;
+    }
+
+    // Convert radians to degrees
+    compassHeading *= 180 / Math.PI;
+    return compassHeading;
   }
 }
