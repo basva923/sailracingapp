8266d431286d5d36a0d7dc2e71b4931e11cb2db5
Bas Van Assche <basvanassche@gmail.com>
2024-05-12 18:55:58 +0200

    Added timer screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,67 +1,15 @@
 import { Component } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { TimerComponent } from '../timer/timer.component';
+import { WindComponent } from '../wind/wind.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TimerComponent, NgbNavModule],
+  imports: [TimerComponent, NgbNavModule, WindComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  absolute = false;
-  alpha: number = 0;
-  beta: number = 0;
-  gamma: number = 0;
   activeTab = 1;
-
-  constructor() {
-    window.addEventListener(
-      'deviceorientationabsolute',
-      (e) => this.handleOrientationChange(e),
-      true
-    );
-  }
-
-  handleOrientationChange(event: DeviceOrientationEvent) {
-    this.absolute = event.absolute;
-    if (event.alpha != null) this.alpha = event.alpha;
-    if (event.beta != null) this.beta = event.beta;
-    if (event.gamma != null) this.gamma = event.gamma;
-  }
-
-  get heading() {
-    // Convert degrees to radians
-    var alphaRad = this.alpha * (Math.PI / 180);
-    var betaRad = this.beta * (Math.PI / 180);
-    var gammaRad = this.gamma * (Math.PI / 180);
-
-    // Calculate equation components
-    var cA = Math.cos(alphaRad);
-    var sA = Math.sin(alphaRad);
-    var cB = Math.cos(betaRad);
-    var sB = Math.sin(betaRad);
-    var cG = Math.cos(gammaRad);
-    var sG = Math.sin(gammaRad);
-
-    // Calculate A, B, C rotation components
-    var rA = -cA * sG - sA * sB * cG;
-    var rB = -sA * sG + cA * sB * cG;
-    var rC = -cB * cG;
-
-    // Calculate compass heading
-    var compassHeading = Math.atan(rA / rB);
-
-    // Convert from half unit circle to whole unit circle
-    if (rB < 0) {
-      compassHeading += Math.PI;
-    } else if (rA < 0) {
-      compassHeading += 2 * Math.PI;
-    }
-
-    // Convert radians to degrees
-    compassHeading *= 180 / Math.PI;
-    return compassHeading;
-  }
 }
--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,12 +1,147 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-timer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './timer.component.html',
-  styleUrl: './timer.component.css'
+  styleUrl: './timer.component.css',
 })
 export class TimerComponent {
+  startTime: Date | null = null;
+  timeLeft: string = '---';
+  distanceToLine: string = '---';
+  pinEndPosition: GeolocationCoordinates | null = null;
+  boadEndPosition: GeolocationCoordinates | null = null;
 
+  constructor() {
+    const self = this;
+    setInterval(() => {
+      self.calcTimeLeft();
+      self.calcDistanceToLine();
+    }, 500);
+  }
+
+  startInMinutes(minutes: number) {
+    this.startTime = new Date();
+    this.startTime.setMinutes(this.startTime.getMinutes() + minutes);
+    this.calcTimeLeft();
+  }
+
+  syncToClosestMinute() {
+    const now = new Date();
+    if (this.startTime) {
+      const secondsOfMinute = Math.floor(
+        (this.timediffInMilliseconds() % 60000) / 1000
+      );
+      if (secondsOfMinute >= 30) {
+        this.startTime.setSeconds(
+          this.startTime.getSeconds() + 60 - secondsOfMinute
+        );
+      } else {
+        this.startTime.setSeconds(
+          this.startTime.getSeconds() - secondsOfMinute
+        );
+      }
+    }
+    this.calcTimeLeft();
+  }
+
+  reset() {
+    this.startTime = null;
+    this.calcTimeLeft();
+  }
+
+  calcTimeLeft() {
+    if (this.startTime) {
+      const diff = this.timediffInMilliseconds();
+
+      const minutes = Math.floor(diff / 60000);
+      const seconds = Math.floor((diff % 60000) / 1000);
+      this.timeLeft = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    } else {
+      // return current time
+      const now = new Date();
+      this.timeLeft = `${now.getHours()}:${now.getMinutes()}`;
+    }
+  }
+
+  private timediffInMilliseconds(): number {
+    if (this.startTime) {
+      const now = new Date();
+      const diff = Math.abs(this.startTime.getTime() - now.getTime());
+      return diff;
+    }
+    return 0;
+  }
+
+  togglePinEndPositionToCurrentLocation() {
+    if (this.pinEndPosition) {
+      this.pinEndPosition = null;
+      this.calcDistanceToLine();
+    } else {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.pinEndPosition = position.coords;
+          this.calcDistanceToLine();
+        },
+        (error) => {},
+        { enableHighAccuracy: true }
+      );
+    }
+  }
+
+  toggleBoadEndPositionToCurrentLocation() {
+    if (this.boadEndPosition) {
+      this.boadEndPosition = null;
+      this.calcDistanceToLine();
+    } else {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.boadEndPosition = position.coords;
+          this.calcDistanceToLine();
+        },
+        (error) => {},
+        { enableHighAccuracy: true }
+      );
+    }
+  }
+
+  calcDistanceToLine() {
+    const self = this;
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        if (self.pinEndPosition && self.boadEndPosition) {
+          self.distanceToLine =
+            this.distancePointToLine(
+              position.coords.latitude,
+              position.coords.longitude,
+              self.pinEndPosition.latitude,
+              self.pinEndPosition.longitude,
+              self.boadEndPosition.latitude,
+              self.boadEndPosition.longitude
+            ).toFixed(0) + 'm';
+        } else {
+          this.distanceToLine = '---';
+        }
+      },
+      (error) => {},
+      { enableHighAccuracy: true }
+    );
+  }
+
+  distancePointToLine(
+    x0: number,
+    y0: number,
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number
+  ) {
+    return (
+      Math.abs((x2 - x1) * (y1 - y0) - (x1 - x0) * (y2 - y1)) /
+      Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
+    );
+  }
 }
