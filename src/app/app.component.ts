fd8423147bfbdb124e14ace70b62eb9d3beda4d2
Bas Van Assche <basvanassche@gmail.com>
2024-09-07 08:16:39 +0200

    Make screen always on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,18 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'sailracingapp';
 
- 
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.requestAlwaysOn();
+  }
+
+  async requestAlwaysOn() {
+    try {
+      const wakeLock = await navigator.wakeLock.request('screen');
+    } catch (err: any) {
+      // the wake lock request fails - usually system related, such being low on battery
+      console.log(`${err.name}, ${err.message}`);
+    }
+  }
 }
